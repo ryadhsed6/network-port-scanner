34f21920a9ab1526728ee5a04a1f62068561c50e
ryadhsed6 <s.ryadh15@gmail.com>
2026-04-15 15:30:48 -0700

    upgrade CLI port scanner with threading and export

--- scanner.py
+++ scanner.py
@@ -1,8 +1,19 @@
+import argparse
 import socket
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 
-target = input("Enter target IP or domain: ")
+parser = argparse.ArgumentParser(description="Simple Python Port Scanner")
+
+parser.add_argument("-t", "--target", help="Target IP or domain", required=True)
+parser.add_argument("-p", "--ports", help="Port range (default 1-1000)", default="1-1000")
+
+args = parser.parse_args()
+
+target = args.target
+port_range = args.ports
+
+start_port, end_port = map(int, port_range.split("-"))
 
 print("-" * 50)
 print(f"Scanning target: {target}")
@@ -19,25 +30,21 @@ print(f"IP Address: {target_ip}")
 print("-" * 50)
 
 # Function to scan a single port
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
-open_ports = []
-
 def scan_port(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.3)
 
     result = sock.connect_ex((target_ip, port))
-
     sock.close()
 
     if result == 0:
         return port
     return None
 
+open_ports = []
 
 with ThreadPoolExecutor(max_workers=200) as executor:
-    results = executor.map(scan_port, range(1, 1001))
+    results = executor.map(scan_port, range(start_port, end_port + 1))
 
     for port in results:
         if port is not None:
@@ -46,16 +53,22 @@ with ThreadPoolExecutor(max_workers=200) as executor:
 print("\nRESULTS:")
 print("-" * 30)
 
+# Saves the results in file named results.txt
+with open("results.txt", "w") as file:
+    file.write(f"Scan results for {target} ({target_ip})\n")
+    file.write("-" * 40 + "\n")
+
+    for port in open_ports:
+        file.write(f"OPEN PORT: {port}\n")
+
+    file.write("-" * 40 + "\n")
+    file.write(f"Total OPEN ports: {len(open_ports)}\n")
+
 for port in open_ports:
     print(f"[OPEN] Port {port}")
 
 print("-" * 30)
 print(f"Total OPEN ports: {len(open_ports)}")
-
-# Run multiple threads
-with ThreadPoolExecutor(max_workers=100) as executor:
-    for port in range(1, 1001):
-        executor.submit(scan_port, port)
 
 print("-" * 50)
 print("Scan completed.")
